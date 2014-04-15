ed5deeb25ae49d9085d8e67c0cc29b28aac76db2
Soffel <andre.wagenknecht@gmx.net>
2014-04-15 21:47:37 +0200

    codestyle modifications, revised clientstop

--- Server/src/de/blauePandas/LMSServer/core/localhost_connector.java
+++ Server/src/de/blauePandas/LMSServer/core/localhost_connector.java
@@ -2,18 +2,18 @@ package de.blauePandas.LMSServer.core;
 
 /**
  * @author havoc
- * 
+ *
  * packt diese Datei am besten in euren .gitignore Ordner,
  * sie ist NUR dazu da, unsere localhost-mysql-server
  * mit jeweils anderen login-daten zu verwalten.
- * 
+ *
  * Sprich: tragt hier eure Login-Daten vom MySQL ein
  * und pusht die Datei auf keinen Fall ins github
- * 
+ *
  * Die Datei wird beim Serverstart in den ConnectionPool eingelesen
  */
 public class localhost_connector {
-    public static final String _HOSTNAME_ = "localhost/PRG4";
+    public static final String _HOSTNAME_ = "localhost/prgjava";
     public static final String _USERNAME_ = "root";
     public static final String _PASSWORD_ = "";
 }
