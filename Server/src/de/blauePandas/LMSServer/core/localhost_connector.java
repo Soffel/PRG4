01b52667b2f71dad528017b8fab698080651cb7b
Soffel <andre.wagenknecht@gmx.net>
2014-04-19 23:24:27 +0200

    codestyle modifications, revised clientstop

--- Server/src/de/blauePandas/LMSServer/core/localhost_connector.java
+++ Server/src/de/blauePandas/LMSServer/core/localhost_connector.java
@@ -12,7 +12,8 @@ package de.blauePandas.LMSServer.core;
  *
  * Die Datei wird beim Serverstart in den ConnectionPool eingelesen
  */
-public class localhost_connector {
+public class localhost_connector
+{
     public static final String _HOSTNAME_ = "localhost/prgjava";
     public static final String _USERNAME_ = "root";
     public static final String _PASSWORD_ = "";
