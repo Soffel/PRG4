5dc5eac8916545a30bf1362dda4f2c3c62b69993
Martin <martinguzien@yahoo.de>
2014-07-18 09:51:34 +0200

    set/get geschrieben

--- Server/src/de/blauePandas/LMSServer/model/Person.java
+++ Server/src/de/blauePandas/LMSServer/model/Person.java
@@ -14,4 +14,42 @@ public class Person
     private String loginName;
     private String loginPsw;
 
+
+    /**
+     * getter
+     */
+    public int getId()
+    {
+        return this.id;
+    }
+
+    public int getRights() { return this.rights; }
+
+    public String getName() { return this.name; }
+
+    public String getloginName () { return this.loginName; }
+
+    public String getloginPsw() { return this.loginPsw; }
+
+
+    /**
+     * Setter
+     */
+
+    public void setId(int _id) { this.id = _id; }
+
+    public void setRights(int _rights) { this.rights = _rights; }
+
+    public void setName(String _name) { this.name = _name; }
+
+    public void setLoginName(String _loginName)
+    {
+        this.loginName = _loginName;
+    }
+
+    public void setLoginPsw(String _loginPsw)
+    {
+        this.loginPsw = _loginPsw;
+    }
 }
+
